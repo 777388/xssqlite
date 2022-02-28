9f7c9271a377e51d3fd81ae3305ccdfd62af68d3
777388 <96343159+777388@users.noreply.github.com>
2022-02-28 15:29:41 -0800

    Add files via upload

--- xssqlite.py
+++ xssqlite.py
@@ -1,4 +1,4 @@
 import os
 import sys
-f = ("go run crawl.go "+sys.argv[1]+" "+sys.argv[2]+" | python xssql2.py &&"+" go run xssql.go 2"+sys.argv[2]+" | python xssql2.py")
+f = ("python Sublist3r/sublist3r.py -d "+sys.argv[3]+ " -o "+sys.argv[1]+" && go run crawl.go "+sys.argv[1]+" "+sys.argv[2]+" | python xssql2.py ;"+" go run xssql.go 2"+sys.argv[2]+" | python xssql2.py")
 os.system(f)
